1302a056feb3675d04c4c259028a31754d6d9f6f
Alexarnd <alex-neve@yandex.com>
2019-06-03 14:40:58 +0300

    ReactJS component created

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
+
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.send('привет');
+});
+
+app.use(function(req, res, next) {
+  next(createError(404, 'Not Found', { expose: true }));
 });
 
 app.listen(8888, function () {
